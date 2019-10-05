5468e8e806e1502e97974d91997fd50505a209bf
RxnNode <rxnnode@gmail.com>
2019-10-05 13:33:16 +0800

    Update navbar menu

--- js/load.js
+++ js/load.js
@@ -8,7 +8,9 @@ $(document).ready(function () {
   $('html').click(function (event) {
     var clickover = $(event.target);
     var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse show");
-    if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+    console.log(clickover);
+
+    if (_opened === true && (!clickover.hasClass("navbar-toggler ml-auto NavTogglerStyle") && !clickover.hasClass("navbar-toggler-icon"))) {
         CollapseNavbar();
     }
   });

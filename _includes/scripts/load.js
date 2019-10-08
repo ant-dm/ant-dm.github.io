5d42c562d0a50b9f9b8eacee6cbb85d8a4924420
RxnNode <rxnnode@gmail.com>
2019-10-08 15:41:21 +0800

    update

--- _includes/scripts/load.js
+++ _includes/scripts/load.js
@@ -1,20 +1,18 @@
 $(document).ready(function () {
   //Collapse Navbar when click outside
+  var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse show");
   function CollapseNavbar() {
     $(".navbar-collapse").removeClass('show');
     $(".navbar-toggle").addClass('collapsed');
   }
-
   $('html').click(function (event) {
     var clickover = $(event.target);
-    var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse show");
     console.log(clickover);
-
     if (_opened === true &&
         (!clickover.hasClass("navbar-toggler ml-auto NavTogglerStyle") &&
          !clickover.hasClass("navbar-toggler-icon") &&
          !clickover.hasClass("nav-link") &&
-         !clickover.is("p"))) {
+         !clickover.hasClass("menu")) ){
         CollapseNavbar();
     }
   });
@@ -22,6 +20,11 @@ $(document).ready(function () {
   var def = 0;
   $(window).on('scroll', function() {
     $('.navbar-wapper').toggleClass('scrollhide', $(window).scrollTop() > def);
-    def = $(window).scrollTop();
+    if (_opened == true) {
+      $(".navbar-wapper").removeClass('scrollhide');
+    } else {
+      def = $(window).scrollTop();
+    }
+
   })
 });
